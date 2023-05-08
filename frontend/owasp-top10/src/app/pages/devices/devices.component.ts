d9875b77d161d3cb7a27e9924fc4aaf3ddaa7bb2
Marcel-TO <marcel@turobin-ort.at>
2023-05-08 10:42:40 +0200

    added different content if logged in or not

--- frontend/owasp-top10/src/app/pages/devices/devices.component.ts
+++ frontend/owasp-top10/src/app/pages/devices/devices.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss']
 })
-export class DevicesComponent {
+export class DevicesComponent implements OnInit {
   public degree = 20
+  public currentUser = undefined;
+  
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   public onIncreaseTemp() {
     if (this.degree < 30) {
@@ -18,7 +22,27 @@ export class DevicesComponent {
     if (this.degree > 14) {
       this.degree -= 1;
     }
-
   }
 
+  ngOnInit(): void {
+    this.currentUser = this.activatedRoute.snapshot.queryParams['username'];
+    let devices = document.getElementById('devices');
+    let unsigned = document.getElementById('unsignedDevices');
+    console.log(this.currentUser)
+    console.log(unsigned)
+    if (this.currentUser == undefined) {
+      if (devices != null) {
+        devices.style.display = 'none'
+      }
+      if (unsigned != null) {
+        unsigned.style.display = 'unset'
+        console.log('changed')
+      }
+    }
+    else {
+      if (unsigned != null) {
+        unsigned.style.display = 'none'
+      }
+    }
+  }
 }
--- frontend/owasp-top10/src/app/pages/history/history.component.ts
+++ frontend/owasp-top10/src/app/pages/history/history.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  public currentUser = undefined;
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.activatedRoute.snapshot.queryParams['username'];
+    // let history = document.getElementById('history');
+    let unsigned = document.getElementById('unsignedHistory');
+    console.log(this.currentUser)
+    if (this.currentUser == undefined) {
+      // if (history != null) {
+      //   history.style.display = 'none'
+      // }
+      if (unsigned != null) {
+        unsigned.style.display = 'unset'
+      }
+    }
+    else {
+      if (unsigned != null) {
+        unsigned.style.display = 'none'
+      }
+    }
+  }
 }
