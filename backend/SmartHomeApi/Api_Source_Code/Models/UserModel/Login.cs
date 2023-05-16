4f1c26b109db5b08def0d41b69a7a02a201cb0a2
Kariyampalli <christyk2001@gmail.com>
2023-05-16 09:07:05 +0200

    First version, models and api

--- backend/SmartHomeApi/Api_Source_Code/Models/UserModel/Login.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartHomeApi.Api_Source_Code.Models.UserModel;
-public class Login{
-    public string UserName{get;set;}
-    public string Password{get;set;}
-    public Login(string userName, string password){
-        this.UserName = userName;
-        this.Password = password;
-    }
-}
--- backend/SmartHomeApi/StartUp.cs
+++ backend/SmartHomeApi/StartUp.cs
@@ -1,15 +1,29 @@
-using Microsoft.EntityFrameworkCore;
 using SmartHomeApi.Api_Source_Code.Config;
 using SmartHomeApi.New.Contexts;
 using SmartHomeApi.New.Repositories.Wrapper;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+
 public static class StartUp{
 
     public static void Run(string[] args){
         var builder = WebApplication.CreateBuilder(args);
         string policy = "policy";
-        //var connectionString  = builder.Configuration.GetConnectionString("DefaultConnection");
-        //builder.Services.AddDbContext<SHDbContext>(x => x.UseSqlServer(connectionString));
-        //builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+       var connectionString  = builder.Configuration.GetConnectionString("server=localhost;port=3306;database=smart_home_db;user=root;password=password");
+        var cs2 = "Server=localhost,3306;Database=smart_home_db;User Id=root;Password=password;";
+        
+        
+         builder.Services.AddDbContextPool<SHDbContext>(
+            options => options.UseMySql(cs2,new MySqlServerVersion(new Version(8, 0, 33))));
+         builder.Services.AddMvc();
+      
+        //builder.Services.AddDbContext<SHDbContext>(x => x.UseSqlServer(),
+            //providerOptionSystem.TypeLoadException: Method 'Create' in type 'MySql.Data.EntityFrameworkCore.Query.Internal.MySQLSqlTranslatingExpressionVisitorFactors => providerOptions.EnableRetryOnFailur());
+        builder.Services.AddTransient<IRepositoryWrapper, RepositoryWrapper>();
+
         ServiceConfig.Config(builder.Services, policy);
        
         var app = builder.Build();
